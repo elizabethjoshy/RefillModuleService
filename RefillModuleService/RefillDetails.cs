27b909f21b9183b5b1e8097857f3b70eb5ba95c8
elizabethjoshy <37285004+elizabethjoshy@users.noreply.github.com>
2021-04-19 13:24:38 +0530

    Add files via upload

---
+++ RefillModuleService/RefillDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefillModuleService
+{
+    public class RefillDetails
+    {
+        public int RefillOrderId { get; set; }
+        public int Subscription_ID { get; set; }
+        public int Member_ID { get; set; }
+        public string DrugName { get; set; }
+        public string Location { get; set; }
+        public int Quantity { get; set; }
+
+        public int NoOfRefills { get; set; }
+        public DateTime FirstRefillDate { get; set; }
+        public DateTime LastRefillDate { get; set; }
+     
+       public DateTime PrevRefillDate { get; set; }
+
+        public DateTime NextRefillDate { get; set; }
+
+        public string RefillOccurnace { get; set; }
+        // public int Policy_ID { get; set; }
+        public string Status { get; set; }
+
+     
+       
+
+
+    }
+}
